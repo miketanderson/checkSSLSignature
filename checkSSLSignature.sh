b54c020aba6dc1cef8c403f7e6acef74fa0e6397
Mike Anderson <miketanderson@gmail.com>
2014-09-09 09:07:07 -0400

    first version of the script

---
+++ checkSSLSignature.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#default port = 443
+PORT=443
+
+usage () {
+  echo "Two arguments required"
+  echo "Usage: checkSSLSignature.sh www.website.com output_certificate_file"
+}
+
+
+
+if (( $# != 2))
+then
+  usage
+  exit 1
+fi
+
+echo -n | openssl s_client -connect $1:$PORT 2> /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END    CERTIFICATE-/p' > $2
+openssl x509 -noout -text < $2 | grep "Signature Algorithm"
+
+
+
+
